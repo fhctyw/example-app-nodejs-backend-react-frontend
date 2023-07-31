4547696a70930a93ec04e76adbe2b84ee014dd65
fhctyw <lisnychukarsenw@gmail.com>
2023-08-01 01:00:42 +0300

    name fix)

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>Welcome to My Project. Arsen Lisynuchk</h1>;
+	return <h1>Welcome to My Project. Arsen Lisnychuk</h1>;
 }
